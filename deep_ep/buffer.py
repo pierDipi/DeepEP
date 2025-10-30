60052abf0a0c8d443d0a82de47ef5a5ee4355787
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-10-30 10:40:32 +0100

    Revert "Preserve NVSHMEM_IBGDA_NUM_RC_PER_PE"
    This reverts commit 2a740d9ebd46d811b287a194836a4b6920d4955c.

--- deep_ep/buffer.py
+++ deep_ep/buffer.py
@@ -101,7 +101,7 @@ class Buffer:
             assert num_qps_per_rank > 0
             os.environ['NVSHMEM_DISABLE_P2P'] = '0' if allow_nvlink_for_low_latency_mode else '1'
             os.environ['NVSHMEM_IB_ENABLE_IBGDA'] = '1'
-            os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] = os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] if os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] is not None else f'{num_qps_per_rank}'
+            os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] = f'{num_qps_per_rank}'
 
             # Make sure QP depth is always larger than the number of on-flight WRs, so that we can skip WQ slot check
             self.nvshmem_qp_depth = int(os.environ.get('NVSHMEM_QP_DEPTH', '1024'))
